8291f89f761dbdde9e0a57fd64af8e9134360504
Pova <malasgharian@gmail.com>
2022-10-08 20:26:49 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+setInterval(function time(){
+  var d = new Date();
+  var hours = 24 - d.getHours();
+  var min = 60 - d.getMinutes();
+  if((min + '').length == 1){
+    min = '0' + min;
+  }
+  var sec = 60 - d.getSeconds();
+  if((sec + '').length == 1){
+        sec = '0' + sec;
+  }
+  jQuery('#upload-counter').html('New Soul well Guy Youtube Upload in '+hours+':'+min+':'+sec)
+}, 1000);
